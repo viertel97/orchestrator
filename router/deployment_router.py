4a7f4b9c54699a24f930aa49620700c9622d2894
viertel97 <janik.viertel@outlook.com>
2023-04-18 15:44:39 +0200

    added more request validation

--- router/deployment_router.py
+++ router/deployment_router.py
@@ -1,6 +1,7 @@
 import hmac
 
 from fastapi import APIRouter, Request
+from fastapi import HTTPException
 from quarter_lib.akeyless import get_secrets
 from quarter_lib.logging import setup_logging
 
@@ -16,6 +17,8 @@ webhook_secret = get_secrets(["github/webhook_secret"])
 @logger.catch
 @router.post("/{application}")
 async def deploy(application: str, request: Request):
+    if not request.headers["X-Hub-Signature-256"]:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
     logger.info("received request for application: %s", application)
     body = await request.body()
     expected_signature = "sha256=" + hmac.new(webhook_secret.encode(), body, "sha256").hexdigest()
