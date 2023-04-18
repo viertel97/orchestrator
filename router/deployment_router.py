2730d31ba0669e46c8ed0643a4fc2b01e776b84b
viertel97 <janik.viertel@outlook.com>
2023-04-18 15:12:06 +0200

    added request validation

--- router/deployment_router.py
+++ router/deployment_router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+import hmac
+
+from fastapi import APIRouter, Request
+from quarter_lib.akeyless import get_secrets
 from quarter_lib.logging import setup_logging
 
 from helper.deployment_helper import update_application
@@ -7,10 +10,18 @@ logger = setup_logging(__file__)
 
 router = APIRouter()
 
+webhook_secret = get_secrets(["github/webhook_secret"])
+
 
 @logger.catch
 @router.post("/{application}")
-async def deploy(application: str):
+async def deploy(application: str, request: Request):
     logger.info("received request for application: %s", application)
+    body = await request.body()
+    expected_signature = "sha256=" + hmac.new(webhook_secret.encode(), body, "sha256").hexdigest()
+    signature = request.headers["X-Hub-Signature-256"]
+    if not hmac.compare_digest(expected_signature, signature):
+        logger.error("signature mismatch")
+        return
     update_application(application)
     logger.info("done")
